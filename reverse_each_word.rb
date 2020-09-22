c1783af94e77b5b2fc9b0f359b279e288afe193c
Luis Penaloza <ldp231@yahoo.com>
2020-09-22 20:27:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(string)
   arr = string.split(" ")
-  new_arr = arr.collect {|a| a.reverse}
-  new_arr.join(" ")
-end
-
-#def reverse_each_word(string)
-#  (string.split(' ')).collect do |element|
- #   element.reverse!
- # end.join("")
-#end
+  #new_arr = arr.collect {|a| a.reverse}
+ #new_arr.join(" ")
+end 
