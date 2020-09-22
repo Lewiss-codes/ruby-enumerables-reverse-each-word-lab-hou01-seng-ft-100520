c28ae8bca7cb59ce53e0ee576c767ecfd197ccd0
Luis Penaloza <ldp231@yahoo.com>
2020-09-22 20:38:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,7 @@ def reverse_each_word(string)
   array = string.split("")
   new_array = [ ]
   array.each do |element| 
-    element.reverse!
+    element.reverse! << new_array
+    
   end.join(" ")
 end
